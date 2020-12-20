9cbed252a128468543a3818b6109058b97e7cecf
shaohailin <shaohailin@jiuqi.com.cn>
2020-12-20 11:03:00 +0800

    初始化配置

---
+++ nginx/os.config.js
@@ -0,0 +1,9 @@
+window.osConfig = {
+  baseUrl: "http://gcreport-web-forward-hyhbbb.apps.paas.cnooc" ,
+  thin: false,
+  cookieExpires: 1,
+  storageMode: "sessionStorage", // auto -- cookie sessionStorage -- sessionStorage localStorage -- localStorage
+  title: "", //ä¯ÀÀÆ÷title
+  mode: "",
+  systemName: ''
+};
